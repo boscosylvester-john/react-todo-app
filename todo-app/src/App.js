cc25dc96d0b8c8c704ca14776bedc5aa12214830
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2023-05-21 00:57:37 -0400

    Add navigation bar

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainContent from './components/mainContent';
+import Navigation from './components/navigation';
+
+const App = () => {
+	return (
+		<div>
+			<Navigation />
+			<MainContent />
+		</div>
+	)
+}
+
+export default App;
---
+++ todo-app/src/components/navigation.js
@@ -0,0 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from "react-bootstrap/Nav"
+
+
+
+const Navigation = () => {
+	return (
+		<Navbar bg='dark' variant='dark'>
+			<Container>
+				<Navbar.Brand href="#home">ToDo App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Tasks</Nav.Link>
+            <Nav.Link href="#features">My Day</Nav.Link>
+						<Nav.Link href="#features">Missed Tasks</Nav.Link>
+            <Nav.Link href="#pricing">Favorites</Nav.Link>
+						<Nav.Link href="#features">Completed</Nav.Link>
+          </Nav>
+			</Container>
+		</Navbar>
+	)
+}
+
+export default Navigation
