5e7a99862fa02b7a6123ff230ba0663900506436
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2023-05-29 09:14:43 -0400

    Add functionality to clear search key

--- todo-app/src/components/Searcher.js
+++ todo-app/src/components/Searcher.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import searcherStyles from './Searcher.module.css';
+import { AiOutlineCloseSquare } from 'react-icons/ai';
 
 const Searcher = ({ searchTasks }) => {
   const [searchKey, setSearchKey] = useState('');
@@ -10,17 +11,25 @@ const Searcher = ({ searchTasks }) => {
   }, [searchKey]);
 
   return (
-    <div className={searcherStyles.searchInput}>
-      <Form.Group>
-        <Form.Control
-          type="input"
-          placeholder="Search with a keyword"
-          value={searchKey}
-          onChange={(event) => {
-            setSearchKey(event.target.value);
-          }}
-        />
-      </Form.Group>
+    <div className={searcherStyles.search}>
+      <div className={searcherStyles.searchInput}>
+        <Form.Group>
+          <Form.Control
+            type="input"
+            placeholder="Search with a keyword"
+            value={searchKey}
+            onChange={(event) => {
+              setSearchKey(event.target.value);
+            }}
+          />
+        </Form.Group>
+      </div>
+      <AiOutlineCloseSquare
+        className={searcherStyles.closeIcon}
+        onClick={() => {
+          setSearchKey('');
+        }}
+      />
     </div>
   );
 };
