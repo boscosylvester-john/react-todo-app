75496309225cdfb5daf2160901d58527f52bcbd8
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2023-05-28 21:30:54 -0400

    Update css for sorter component

--- todo-app/src/components/Sorter.js
+++ todo-app/src/components/Sorter.js
@@ -4,6 +4,7 @@ import {
   SORTING_OPTIONS,
   SORTING_OPTIONS_ARRAY
 } from '../constants';
+import sorterStyles from './Sorter.module.css';
 
 const Sorter = ({ sortTasks }) => {
   const [sorterValue, setSorterValue] = useState(
@@ -15,22 +16,24 @@ const Sorter = ({ sortTasks }) => {
   }, [sorterValue]);
 
   return (
-    <Form.Group className="mb-3">
-      <Form.Label>Sort by</Form.Label>
-      <Form.Select
-        value={sorterValue}
-        onChange={(event) => {
-          setSorterValue(event.target.value);
-        }}>
-        {SORTING_OPTIONS_ARRAY.map((op, index) => {
-          return (
-            <option key={index} value={op}>
-              {op}
-            </option>
-          );
-        })}
-      </Form.Select>
-    </Form.Group>
+    <div className={sorterStyles.sortOptionDropdown}>
+      <Form.Group className="mb-3">
+        <Form.Label>Sort by</Form.Label>
+        <Form.Select
+          value={sorterValue}
+          onChange={(event) => {
+            setSorterValue(event.target.value);
+          }}>
+          {SORTING_OPTIONS_ARRAY.map((op, index) => {
+            return (
+              <option key={index} value={op}>
+                {op}
+              </option>
+            );
+          })}
+        </Form.Select>
+      </Form.Group>
+    </div>
   );
 };
 
